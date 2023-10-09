8370003673238adf36d2f216ef5b341edf619097
davide-rullo <davide.rullo01@gmail.com>
2023-10-09 15:15:44 +0200

    add censor length

--- script.php
+++ script.php
@@ -5,14 +5,24 @@
 $mytext = $_POST['mytext'];
 $badword = $_POST['badword'];
 
-$textlength = strlen($mytext);
+$badwordlen = strlen($badword);
 
+$hidden = str_repeat('***', $badwordlen);;
+
+$textlength = strlen($mytext) + strlen($badword);
+$textcensorlen = strlen($mytext) + strlen($hidden);
 ?>
 
 <body>
+    <h2>Paragrafo:</h2>
+    <p><?php echo "$mytext $badword"; ?></p>
 
-    <h2><?php echo $mytext; ?></h2>
+    <h2>Numero di lettere nel paragrafo: <?php echo $textlength; ?> </h2>
+    <br><br><br>
 
-    <h2><?php echo $textlength; ?> </h2>
 
+
+    <p><?php echo "$mytext $hidden"; ?></p>
+
+    <h2>Numero di lettere nel paragrafo: <?php echo $textcensorlen; ?> </h2>
 </body>
